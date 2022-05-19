48c70c6bd51ce2f85239de0700a284020dfb4d5c
Guilherme Alves <guiigos.alves@gmail.com>
2022-05-19 12:14:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import http from 'http';
 import app from './src/app.js';
 
 const server = http.createServer(app);
-server.listen(3030 || process.env.PORT);
+server.listen(3000 || process.env.PORT);
